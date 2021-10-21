5698f2ef85115af1100a18bba2b164ab90452b20
danobohud <danobohud@gmail.com>
2021-10-21 19:06:58 +0100

    add autodocs capability

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,14 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../pkmodel2-main'))
+sys.path.insert(0, os.path.abspath('...'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PK Project'
-copyright = '2021, Todo'
-author = 'Todo'
+project = 'pk project'
+copyright = '2021, todo'
+author = 'todo'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1.0'
@@ -30,8 +30,8 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- docs/index.rst
+++ docs/index.rst
@@ -1,16 +1,14 @@
-.. PK Project documentation master file, created by
-   sphinx-quickstart on Thu Oct 21 17:47:26 2021.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to PK Project's documentation!
+Welcome to pk project's documentation!
 ======================================
+.. automodule:: pkmodel2-main.solver
+    :members:
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-Input text here
+
+Hello World!
 
 Indices and tables
 ==================
